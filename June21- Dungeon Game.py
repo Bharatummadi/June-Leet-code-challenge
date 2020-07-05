cd5edb914abfe2ca144748c31f275ff809f401c5
Bharatummadi <bharatummadi23@gmail.com>
2020-07-05 11:21:28 +0530

    Added Day21

--- June21- Dungeon Game.py
+++ June21- Dungeon Game.py
@@ -7,5 +7,4 @@ class Solution:
         for i in range(m-1,-1,-1):
             for j in range(n-1,-1,-1):
                 dp[i][j] = max(min(dp[i+1][j],dp[i][j+1])-dungeon[i][j],1)
-        
         return dp[0][0]
